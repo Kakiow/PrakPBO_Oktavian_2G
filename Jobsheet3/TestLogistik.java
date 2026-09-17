1d51c5e2854c5957518d424d71eb3032d78e89f2
Kakiow <xcexc111@gmail.com>
2026-09-17 22:24:48 +0700

    Tugas no 6

--- Jobsheet3/TestLogistik.java
+++ Jobsheet3/TestLogistik.java
@@ -1,26 +1,33 @@
 package Jobsheet3;
 
+import java.util.Scanner;
+
 public class TestLogistik {
     public static void main(String[] args) {
+     Scanner okta = new Scanner(System.in);
      Kontainer kontainerAlfa = new Kontainer("REQ-9988", "PT. Maju Bersama", 5000);
      
      System.out.println("Nama Pemilik Kontainer: " + kontainerAlfa.getNamaPemilik());
      System.out.println("Kapasitas Maksimal: " + kontainerAlfa.getKapasitasMaksimal() + " kg");
 
-     System.out.println("\nMemasukkan muatan baru seberat 6.000 kg...");
-     kontainerAlfa.tambahMuatan(6000);
+     System.out.println("\nMemasukkan muatan baru");
+     double muat1 = okta.nextDouble();
+     kontainerAlfa.tambahMuatan(muat1);
      System.out.println("Berat muatan saat ini: " + kontainerAlfa.getBeratMuatanSaatIni() + " kg");
 
-     System.out.println("\nMemasukkan muatan baru seberat 4.000 kg...");
-     kontainerAlfa.tambahMuatan(4000);
+     System.out.println("\nMemasukkan muatan baru");
+     double muat2 = okta.nextDouble();
+     kontainerAlfa.tambahMuatan(muat2);
      System.out.println("Berat muatan saat ini: " + kontainerAlfa.getBeratMuatanSaatIni() + " kg");
 
-     System.out.println("\nMembongkar muat/menurunkan barang seberat 500 kg...");
-     kontainerAlfa.turunkanMuatan(500);
+     System.out.println("\nMembongkar muat/menurunkan barang seberat");
+     double bongkar1 = okta.nextDouble();
+     kontainerAlfa.turunkanMuatan(bongkar1);
      System.out.println("Berat muatan saat ini: " + kontainerAlfa.getBeratMuatanSaatIni() + " kg");
 
-     System.out.println("\nMembongkar muat/menurunkan barang seberat 1.500 kg...");
-     kontainerAlfa.turunkanMuatan(1500);
+     System.out.println("\nMembongkar muat/menurunkan barang seberat");
+     double bongkar2 = okta.nextDouble();
+     kontainerAlfa.turunkanMuatan(bongkar2);
      System.out.println("Berat muatan saat ini: " + kontainerAlfa.getBeratMuatanSaatIni() + " kg");
     }
 }
