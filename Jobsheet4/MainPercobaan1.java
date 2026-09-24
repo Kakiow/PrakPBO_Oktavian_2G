f829a01104fcb4547f4579f04ae49beb8372d05e
Kakiow <xcexc111@gmail.com>
2026-09-24 20:02:27 +0700

    Percobaan 1

---
+++ Jobsheet4/MainPercobaan1.java
@@ -0,0 +1,9 @@
+package Jobsheet4;
+
+public class MainPercobaan1 {
+    public static void main(String[] args) {
+        Processor p = new Processor("Intel i5", 3);
+        Laptop l = new Laptop("Thinkpad", p);
+        l.info();
+    }
+}
