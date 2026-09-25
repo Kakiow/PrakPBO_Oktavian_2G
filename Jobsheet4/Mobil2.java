869fd77e8760f2e0c2c29b8a5c52e1cec36b50ff
Kakiow <xcexc111@gmail.com>
2026-09-25 08:12:22 +0700

    Pertanyaan 5 percobaan 5

--- Jobsheet4/Mobil2.java
+++ Jobsheet4/Mobil2.java
@@ -9,6 +9,11 @@ public class Mobil2 {
         this.mesin = new Mesin();
     }
 
+    public Mobil2(String merek, Mesin mesin) {
+        this.merek = merek;
+        this.mesin = mesin;
+    }
+
     public void tampilkanInfo() {
         System.out.println("Mobil: " + merek);
         System.out.println("Mesin: " + mesin.getTipe());
