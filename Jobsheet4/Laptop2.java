c65735d00ab9ff4dabfba29b7d5d63ba3f43c57a
Kakiow <xcexc111@gmail.com>
2026-09-25 08:34:33 +0700

    Pertanyaan 4 percobaan 6

--- Jobsheet4/Laptop2.java
+++ Jobsheet4/Laptop2.java
@@ -2,12 +2,14 @@ package Jobsheet4;
 
 public class Laptop2 {
     private String merk;
+    private Printer printer;
 
-    public Laptop2(String merk) {
+    public Laptop2(String merk, Printer printer) {
         this.merk = merk;
+        this.printer = printer;
     }
 
-    public void cetakDokumen(Printer printer, String namaFile) {
+    public void cetakDokumen(String namaFile) {
         System.out.println(merk + " mengirim dokumen ke printer...");
         printer.cetak(namaFile);
     }
